6ee9237518f7f2b567380b184ed93a4670edcc32
Adrian <quiroga.adrian@gmail.com>
2020-07-18 11:46:16 -0300

    agregué más graficos #1

--- El_Script_del_R.R
+++ El_Script_del_R.R
@@ -1,6 +1,11 @@
 # genero numeros aleatorios y luego los grafico
-numeros <-rnorm(500)
+set.seed(123)
+numeros2 <-rnorm(100)
+numeros1 <-rnorm(500)
 
 # grafico
-hist(numeros)
-plot(numeros)
+hist(numeros1)
+plot(numeros2)
+
+
+
